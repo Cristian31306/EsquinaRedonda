400630219f65f14b8468e54022563ff7e4d2834c
Cristian31306 <est_ca_duran@fesc.edu.co>
2026-03-20 10:44:03 -0500

    ajuste en error usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'email',
         'password',
         'role',
+        'is_active',
     ];
 
     public function isAdmin()
@@ -48,6 +49,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_active' => 'boolean',
         ];
     }
 }
